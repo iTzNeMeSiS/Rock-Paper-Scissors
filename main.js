948f170ec45b2e0ea899b881a0b3093f7b329295
iTzNeMeSiS <codingdre@gmail.com>
2022-07-03 12:23:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,101 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let functions = [showLeftRock, showLeftPaper, showLeftScissors];
 
 function randomNumber(n) {
-    return Math.floor(Math.random() * n);
+    return Math.floor(Math.random() * 3);
 }
 
+globalThis.computerPlay = functions[randomNumber(functions.length)]();
 
 function showRock(){
     document.getElementById('right-rock').style.display = "block";
     document.getElementById('rock').style.display = "none";
     document.getElementById('paper').style.display = "none";
     document.getElementById('scissors').style.display = "none";
-    let usersPlay = "rock";
     functions[randomNumber(functions.length)]();
+    globalThis.usersPlay = "rock";
 }
+
 function showPaper() {
     document.getElementById('right-paper').style.display = "block";
     document.getElementById('rock').style.display = "none";
     document.getElementById('paper').style.display = "none";
     document.getElementById('scissors').style.display = "none";
-    let usersPlay = "paper";
     functions[randomNumber(functions.length)]();
+    globalThis.usersPlay = "paper";
 }
 function showScissors() {
     document.getElementById('right-scissors').style.display = "block";
     document.getElementById('rock').style.display = "none";
     document.getElementById('paper').style.display = "none";
     document.getElementById('scissors').style.display = "none";
-    let usersPlay = "scissors";
     functions[randomNumber(functions.length)]();
+    globalThis.usersPlay = "scissors";
 }
 
 
 function showLeftRock(){
     document.getElementById('left-rock').style.display = "block";
     document.getElementById('rock').style.display = "none";
-    let computerPlay = "rock";
 }
 function showLeftPaper() {
     document.getElementById('left-paper').style.display = "block";
     document.getElementById('paper').style.display = "none";
-    let computersPlay = "paper";
 }
 function showLeftScissors() {
     document.getElementById('left-scissors').style.display = "block";
     document.getElementById('scissors').style.display = "none";
-    let computersPlay = "scissors";
 }
 
+function startRock() {
+    play();
+    setTimeout(function(){ showRock(); }, 2000);
+    setTimeout(function(){ end(); }, 2000);
+    setTimeout(function(){ hide(); }, 4000);
+}
+function startPaper() {
+    play();
+    setTimeout(function(){ showPaper(); }, 2000);
+    setTimeout(function(){ end(); }, 2000);
+    setTimeout(function(){ hide(); }, 4000);
+}
+function startScissors() {
+    play();
+    setTimeout(function(){ showScissors(); }, 2000);
+    setTimeout(function(){ end(); }, 2000);
+    setTimeout(function(){ hide(); }, 4000);
+}
 
-
-
+function end() {
+    document.getElementById('start').style.display = "none";
+    document.getElementById('right-start').style.display = "none";
+}
+function hide() {
+    document.getElementById('right-rock').style.display = "none";
+    document.getElementById('right-paper').style.display = "none";
+    document.getElementById('right-scissors').style.display = "none";
+    document.getElementById('left-rock').style.display = "none";
+    document.getElementById('left-paper').style.display = "none";
+    document.getElementById('left-scissors').style.display = "none";
+    document.getElementById('rock').style.display = "block";
+    document.getElementById('paper').style.display = "block";
+    document.getElementById('scissors').style.display = "block";
+    document.getElementById('winner').style.display = "none";
+}
+function play() {
+    document.getElementById('rock').style.display = "none";
+    document.getElementById('paper').style.display = "none";
+    document.getElementById('scissors').style.display = "none";
+    document.getElementById('start').style.display = "block";
+    document.getElementById('start').style.animation = "spin .5s 3";
+    document.getElementById('right-start').style.display = "block";
+    document.getElementById('right-start').style.animation = "spin .5s 3";
+}
+function tie() {
+    if (usersPlay == computerPlay) {
+        document.getElementById('winner') = "Tie!"
+    }
+}
